e24cda62fdaef4fdd04fbcfb1bfa3ed7f61e2630
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-19 09:11:29 +0300

    Done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
+// function hasTargetSum(array, target) {
+//   // Write your algorithm here
+
+// const seenNumbers={};
+//   for(let i=0;i<array.length;i++){
+//         const compliment=target -array[i]
+//         if (seenNumbers[compliment])return true
+//       seenNumbers[array[i]]= true;
+      
+//       }
+//       return false;
+//     }
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set(); 
+  for (const number of array) {
+    const complement = target - number;
+
+    
+    if (seenNumbers.has(complement)) return true;
+
+    seenNumbers.add(number);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  time complexity:o(n)
+  space complexity:o(n)
+  
 */
 
 /* 
   Add your pseudocode here
+  create an object to keep track of numbers already seen
+  iterate each number in the array
+  check if any key on our object is a compliment
+ 
+  if theres any return true else false
+        using set
+          
+  initialize an empty Set
+  .has returns true if the Set includes the complement
+
+   .add adds the number to the Set
+
+
+
+
 */
 
 /*
   Add written explanation of your solution here
+   create an object to keep track of numbers already seen
+  iterate each number in the array
+  check if any key on our object is a compliment
+ 
+  if theres any return true else false
 */
 
 // You can run `node index.js` to view these console logs
